d37cb4f3f412c79302d095d402f750b34a495fd1
Kimberly Lin <kimberlylin2014@gmail.com>
2021-05-09 12:50:19 -0700

    Updates

---
+++ precache-manifest.fb29d6e62b3b977329fa5c281943e1a7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2815937628c4234c7cde1c56bd79c240",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7aa5bfaa60dc762490f3",
+    "url": "/portfolio/static/css/main.ac4d21c0.chunk.css"
+  },
+  {
+    "revision": "eebecc5638ab2b8d975a",
+    "url": "/portfolio/static/js/2.8c6d1df6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.8c6d1df6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aa5bfaa60dc762490f3",
+    "url": "/portfolio/static/js/main.f084c69e.chunk.js"
+  },
+  {
+    "revision": "b417b3ee81e3e8f9aad5",
+    "url": "/portfolio/static/js/runtime-main.b649af6b.js"
+  },
+  {
+    "revision": "f9e4c6de5137f3668fb13a84a786abfe",
+    "url": "/portfolio/static/media/FilmCritic3.f9e4c6de.png"
+  },
+  {
+    "revision": "4afdaf1d4e00af8af60c2ac399b4db1c",
+    "url": "/portfolio/static/media/HR_Directory1.4afdaf1d.png"
+  },
+  {
+    "revision": "f274c5c6ef8c445b04945a631f22d6c1",
+    "url": "/portfolio/static/media/TrendingInc.f274c5c6.png"
+  },
+  {
+    "revision": "fd58ab2920d5be809379c0f3c33a172d",
+    "url": "/portfolio/static/media/freepik-city.fd58ab29.svg"
+  },
+  {
+    "revision": "a112a001d5170d13d5f46d9387e526a4",
+    "url": "/portfolio/static/media/freepik-github.a112a001.svg"
+  },
+  {
+    "revision": "6ce8ddf9e9e9f21c2c654caa89840d01",
+    "url": "/portfolio/static/media/freepik_gmail.6ce8ddf9.svg"
+  },
+  {
+    "revision": "a8ca8d4e9e7d5a4e70189e422ea6b4bd",
+    "url": "/portfolio/static/media/kimProfilePic.a8ca8d4e.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6bb1256d059b6be465e1d778e0e0096f",
+    "url": "/portfolio/static/media/notepad.6bb1256d.png"
+  },
+  {
+    "revision": "57540e46702d396dd84e34e10c116846",
+    "url": "/portfolio/static/media/study2.57540e46.jpg"
+  },
+  {
+    "revision": "be91ffb0c5b3bf452594a63b737262f6",
+    "url": "/portfolio/static/media/unsplash-city-background.be91ffb0.jpg"
+  }
+]);
